e205edf520abbea80100300484618e81436321e4
Benjamin Lowenthal <62390840+benlowenthal@users.noreply.github.com>
2025-05-26 00:13:23 +0100

    Optimised file reading
    Better coding practices included

--- environment.js
+++ environment.js
@@ -27,7 +27,7 @@ class Material {
 }
 
 
-export const vert = [
+export const vert = new Array(
   new Vector3(-20, 0, -20),
   new Vector3(-20, 0, 0),
   new Vector3(-20, 0, 20),
@@ -37,17 +37,17 @@ export const vert = [
   new Vector3(20, 0, -20),
   new Vector3(20, 0, 0),
   new Vector3(20, 0, 20),
-];
+);
 
-export const normal = [
+export const normal = new Array(
   new Vector3(0, 1, 0)
-];
+);
 
-export const uvs = [
+export const uvs = new Array(
   0.01, 0.01
-];
+);
 
-export const tri = [
+export const tri = new Array(
   new Tri(0, 1, 3, 0, 0, 0, 0, 0, 0, 0),
   new Tri(1, 4, 3, 0, 0, 0, 0, 0, 0, 0),
   new Tri(1, 2, 4, 0, 0, 0, 0, 0, 0, 0),
@@ -56,7 +56,7 @@ export const tri = [
   new Tri(4, 7, 6, 0, 0, 0, 0, 0, 0, 0),
   new Tri(4, 5, 7, 0, 0, 0, 0, 0, 0, 0),
   new Tri(5, 8, 7, 0, 0, 0, 0, 0, 0, 0)
-];
+);
 
 export const mat = [
   new Material("default", 0, 0, 0.7, 0, 1)
@@ -66,55 +66,65 @@ export const tex = [await fetch("missing.jpg").then(r => r.blob())];
 
 
 export async function buildOBJ(blob) {
-  const vertOff = vert.length;
-  const normOff = normal.length;
-  const uvOff = 1;
+  console.time("OBJ parsing");
 
   const txt = await blob.text().then(r => r.split("\n"));
-  var matIdx = 0;
 
-  for (var i = 0; i < txt.length; i++) {
-    var sp = txt[i].trim().split(/\s+/);
+  const vertOff = vert.length;
+  const normOff = normal.length;
+  const uvOff = uvs.length / 2;
 
-    if (sp[0] == "usemtl") {
-      for (var j = 0; j < mat.length; j++) if (mat[j].name == sp[1]) {
-        matIdx = j;
-        break;
-      }
+  //material map for quick lookup
+  const matMap = new Map();
+  for (let j = 0; j < mat.length; j++) matMap.set(mat[j].name, j);
+  let matIdx = 0;
+
+
+  //fill arrays with data
+  for (const line of txt) {
+    var sp = line.trim().split(/\s+/);
+
+    if (sp[0] == "usemtl") matIdx = matMap.get(sp[1]) ?? 0;
+
+    else if (sp[0] == "v") vert.push( new Vector3(parseFloat(sp[1]), parseFloat(sp[2]), -parseFloat(sp[3])) );
+    else if (sp[0] == "vn") normal.push( new Vector3(parseFloat(sp[1]), parseFloat(sp[2]), parseFloat(sp[3])) );
+    else if (sp[0] == "vt") {
+      uvs.push( parseFloat(sp[1]) );
+      uvs.push( parseFloat(sp[2]) );
     }
 
-    else if (sp[0] == "v") vert.push(new Vector3(parseFloat(sp[1]), parseFloat(sp[2]), -parseFloat(sp[3])));
-    else if (sp[0] == "vn") normal.push(new Vector3(parseFloat(sp[1]), parseFloat(sp[2]), parseFloat(sp[3])));
-    else if (sp[0] == "vt") uvs.push(parseFloat(sp[1]), parseFloat(sp[2]));
     else if (sp[0] == "f") {
       var v0 = sp[1].split("/");
       var v1 = sp[2].split("/");
       var v2 = sp[3].split("/");
-      tri.push(new Tri(
+      tri.push( new Tri(
         parseInt(v0[0]) - 1 + vertOff, parseInt(v1[0]) - 1 + vertOff, parseInt(v2[0]) - 1 + vertOff,
         parseInt(v0[2]) - 1 + normOff, parseInt(v1[2]) - 1 + normOff, parseInt(v2[2]) - 1 + normOff,
         parseInt(v0[1]) - 1 + uvOff, parseInt(v1[1]) - 1 + uvOff, parseInt(v2[1]) - 1 + uvOff,
         matIdx
-      ));
+      ) );
 
       //split quad into 2 tris
-      try {
+      if (sp.length > 4) {
         var v3 = sp[4].split("/");
-        tri.push(new Tri(
+        tri.push( new Tri(
           parseInt(v0[0]) - 1 + vertOff, parseInt(v2[0]) - 1 + vertOff, parseInt(v3[0]) - 1 + vertOff,
           parseInt(v0[2]) - 1 + normOff, parseInt(v2[2]) - 1 + normOff, parseInt(v3[2]) - 1 + normOff,
           parseInt(v0[1]) - 1 + uvOff, parseInt(v2[1]) - 1 + uvOff, parseInt(v3[1]) - 1 + uvOff,
           matIdx
-        ));
-      } catch (e) { }
+        ) );
+      }
     }
   }
 
+  console.timeEnd("OBJ parsing");
   console.log(".obj build successful");
 }
 
 
 export async function readMTL(blob) {
+  console.time("MTL parsing");
+
   const txt = await blob.text().then(r => r.split("\n"));
   var matIdx = 0;
 
@@ -136,13 +146,15 @@ export async function readMTL(blob) {
     }
   }
 
-  console.log(mat);
+  console.timeEnd("MTL parsing");
   console.log(".mtl read successful");
 }
 
 export function readTexture(blob) {
+  console.time("Texture parsing");
+
   tex.push(blob);
 
-  console.log(tex);
+  console.timeEnd("Texture parsing");
   console.log("Texture read successful");
 }
